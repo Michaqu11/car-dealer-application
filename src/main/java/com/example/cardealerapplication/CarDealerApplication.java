523cd8c156ffe94a9a836272811a8846fe57cd84
Micha <michaqu11@wp.pl>
2022-10-20 10:44:58 +0200

    refactor code

--- src/main/java/com/example/cardealerapplication/CarDealerApplication.java
+++ src/main/java/com/example/cardealerapplication/CarDealerApplication.java
@@ -2,6 +2,8 @@ package com.example.cardealerapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -11,18 +13,18 @@ public class CarDealerApplication {
         SpringApplication.run(CarDealerApplication.class, args);
     }
 
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("slons", r -> r.host("localhost:8080")
-//                        .and()
-//                        .path("salon/{name}", "/salon")
-//                        .uri("http://localhost:8081"))
-//                .route("car", r -> r.host("localhost:8080")
-//                        .and()
-//                        .path("/car", "/car/**", "/salon/{name}/car", "/salon/{name}/car/**")
-//                        .uri("http://localhost:8082"))
-//                .build();
-//    }
-//
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("salons", r -> r.host("localhost:8080")
+                        .and()
+                        .path("salon/{name}", "/salon")
+                        .uri("http://localhost:8081"))
+                .route("car", r -> r.host("localhost:8080")
+                        .and()
+                        .path("/car", "/car/**", "/salon/{name}/car", "/salon/{name}/car/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
+
 }
